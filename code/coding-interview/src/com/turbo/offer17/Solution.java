041076b8cb20156a5e800e71307027f6bc50db16
yutao <yutao2013@126.com>
2021-07-15 19:06:44 +0800

    offer17

---
+++ code/coding-interview/src/com/turbo/offer17/Solution.java
@@ -0,0 +1,17 @@
+package com.turbo.offer17;
+
+/**
+ *  打印从1到最大的n位数
+ * @author yutao
+ */
+public class Solution {
+
+    public int[] printNumbers(int n){
+        int end = (int) (Math.pow(10,n)- 1);
+        int[] res = new int[end];
+        for(int i=0;i<end;i++){
+            res[i] = i+1;
+        }
+        return res;
+    }
+}
